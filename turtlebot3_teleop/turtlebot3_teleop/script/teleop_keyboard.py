e2a80c89dc6ef7723a8be39c75adaaddffa564c4
zuyuan.zhu <zuyuan.zhu@beko.com>
2024-03-21 13:38:40 +0800

    robot auto follow wall

--- turtlebot3_teleop/turtlebot3_teleop/script/teleop_keyboard.py
+++ turtlebot3_teleop/turtlebot3_teleop/script/teleop_keyboard.py
@@ -1,236 +1,199 @@
-#!/usr/bin/env python
-#
-# Copyright (c) 2011, Willow Garage, Inc.
-# All rights reserved.
-#
-# Software License Agreement (BSD License 2.0)
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of {copyright_holder} nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Darby Lim
+#!/usr/bin/env python3
 
-import os
-import select
-import sys
 import rclpy
-
+from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from rclpy.qos import QoSProfile
+from nav_msgs.msg import Odometry
+from tf_transformations import euler_from_quaternion
+import math
 
-if os.name == 'nt':
-    import msvcrt
-else:
-    import termios
-    import tty
-
-BURGER_MAX_LIN_VEL = 0.22
-BURGER_MAX_ANG_VEL = 2.84
-
-WAFFLE_MAX_LIN_VEL = 0.26
-WAFFLE_MAX_ANG_VEL = 1.82
-
-LIN_VEL_STEP_SIZE = 0.01
-ANG_VEL_STEP_SIZE = 0.1
-
-TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+import math
 
 msg = """
-Control Your TurtleBot3!
----------------------------
-Moving around:
-        w
-   a    s    d
-        x
-
-w/x : increase/decrease linear velocity (Burger : ~ 0.22, Waffle and Waffle Pi : ~ 0.26)
-a/d : increase/decrease angular velocity (Burger : ~ 2.84, Waffle and Waffle Pi : ~ 1.82)
-
-space key, s : force stop
+The robot will move along a predefined path to trigger the telejump.
+The robot's main movements:
+1. Turn 180 degree around.
+2. Move forward with speed 0.26m/s until reaching a specific point.
+3. Turn right until the robot's head direction is parallel to the wall.
+4. Move forward with speed 0.26m/s until robot telejump to the home position. 
 
 CTRL-C to quit
 """
 
-e = """
-Communications Failed
-"""
+FORWARD_SPEED = 0.26  # m/s
+TURN_SPEED = 0.2  # rad/s for angular velocity
+TURN_SPEED_SLOW = 0.1
+angle_tolerance =0.01 # rad for  controlling error
+ROTATE_THRETHOLD = 0.0001  # threshold, robot's stop rotating
+MOVE_THRETHOLD = 0.0001  # threshold, robot's stop moving forward along x
+WALL_ANGLE = -2.10 # angle of the slop wall, robot move along the wall to trigger telejump
+TARGET_POS1_X = -10.18    #-8.26
+TARGET_POS1_Y = 0.47
 
+TARGET_POS2_X = -4.50   #-8.26
+TARGET_POS2_Y = 8.40
 
-def get_key(settings):
-    if os.name == 'nt':
-        return msvcrt.getch().decode('utf-8')
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
-    if rlist:
-        key = sys.stdin.read(1)
-    else:
-        key = ''
+def angle_difference(target, current):
+            diff = target - current
+            # Normalize to [-pi, pi]
+            diff = (diff + math.pi) % (2 * math.pi) - math.pi
+            return abs(diff)
 
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+class AutoMoveBot(Node):
+    def __init__(self):
+        super().__init__('turtlebot3_auto')
+        self.pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.odom_sub = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
+        self.current_pose = None
+        self.is_turning_180 = False
 
+    def odom_callback(self, msg):
+        self.current_pose = msg.pose.pose
 
-def print_vels(target_linear_velocity, target_angular_velocity):
-    print('currently:\tlinear velocity {0}\t angular velocity {1} '.format(
-        target_linear_velocity,
-        target_angular_velocity))
+    def get_yaw_from_pose(self, pose):
+        orientation_q = pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+        return yaw
 
+    def turn_around(self, rotate_yaw=math.pi, clockwise=False):
+        # if self.is_turning:
+        #     return  # Avoid re-entering if already turning
 
-def make_simple_profile(output, input, slop):
-    if input > output:
-        output = min(input, output + slop)
-    elif input < output:
-        output = max(input, output - slop)
-    else:
-        output = input
+        if not self.current_pose:
+            self.get_logger().info('Current pose not available.')
+            return
 
-    return output
+        initial_yaw = self.get_yaw_from_pose(self.current_pose)
+        target_yaw = initial_yaw + rotate_yaw  # Add rotate_yaw degrees in radians
 
+        # Normalize target_yaw to be within [-pi, pi]
+        target_yaw = (target_yaw + math.pi) % (2 * math.pi) - math.pi
 
-def constrain(input_vel, low_bound, high_bound):
-    if input_vel < low_bound:
-        input_vel = low_bound
-    elif input_vel > high_bound:
-        input_vel = high_bound
-    else:
-        input_vel = input_vel
+        # Debug information
+        self.get_logger().info(f'Initial yaw: {initial_yaw:.2f}, Target yaw: {target_yaw:.2f}')
 
-    return input_vel
-
-
-def check_linear_limit_velocity(velocity):
-    if TURTLEBOT3_MODEL == 'burger':
-        return constrain(velocity, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
-    else:
-        return constrain(velocity, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
-
-
-def check_angular_limit_velocity(velocity):
-    if TURTLEBOT3_MODEL == 'burger':
-        return constrain(velocity, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
-    else:
-        return constrain(velocity, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
-
-
-def main():
-    settings = None
-    if os.name != 'nt':
-        settings = termios.tcgetattr(sys.stdin)
-
-    rclpy.init()
-
-    qos = QoSProfile(depth=10)
-    node = rclpy.create_node('teleop_keyboard')
-    pub = node.create_publisher(Twist, 'cmd_vel', qos)
-
-    status = 0
-    target_linear_velocity = 0.0
-    target_angular_velocity = 0.0
-    control_linear_velocity = 0.0
-    control_angular_velocity = 0.0
-
-    try:
-        print(msg)
-        while(1):
-            key = get_key(settings)
-            if key == 'w':
-                target_linear_velocity =\
-                    check_linear_limit_velocity(target_linear_velocity + LIN_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'x':
-                target_linear_velocity =\
-                    check_linear_limit_velocity(target_linear_velocity - LIN_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'a':
-                target_angular_velocity =\
-                    check_angular_limit_velocity(target_angular_velocity + ANG_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'd':
-                target_angular_velocity =\
-                    check_angular_limit_velocity(target_angular_velocity - ANG_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == ' ' or key == 's':
-                target_linear_velocity = 0.0
-                control_linear_velocity = 0.0
-                target_angular_velocity = 0.0
-                control_angular_velocity = 0.0
-                print_vels(target_linear_velocity, target_angular_velocity)
-            else:
-                if (key == '\x03'):
-                    break
-
-            if status == 20:
-                print(msg)
-                status = 0
-
-            twist = Twist()
-
-            control_linear_velocity = make_simple_profile(
-                control_linear_velocity,
-                target_linear_velocity,
-                (LIN_VEL_STEP_SIZE / 2.0))
-
-            twist.linear.x = control_linear_velocity
-            twist.linear.y = 0.0
-            twist.linear.z = 0.0
-
-            control_angular_velocity = make_simple_profile(
-                control_angular_velocity,
-                target_angular_velocity,
-                (ANG_VEL_STEP_SIZE / 2.0))
-
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = control_angular_velocity
-
-            pub.publish(twist)
-
-    except Exception as e:
-        print(e)
-
-    finally:
         twist = Twist()
-        twist.linear.x = 0.0
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
+        if not clockwise:
+            twist.angular.z = TURN_SPEED
+        else:
+            twist.angular.z = -TURN_SPEED
+        self.pub.publish(twist)
 
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = 0.0
+        while rclpy.ok():
+            current_yaw = self.get_yaw_from_pose(self.current_pose)
+            diff = angle_difference(target_yaw, current_yaw)
 
-        pub.publish(twist)
+            # Debug information
+            self.get_logger().info(f'Current yaw: {current_yaw:.2f}, Target yaw: {target_yaw:.2f}, Angle difference: {diff:.2f}')
 
-        if os.name != 'nt':
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+            if diff < TURN_SPEED * 2:  # slow down
+                if not clockwise:
+                    twist.angular.z = TURN_SPEED_SLOW
+                else:
+                    twist.angular.z = -TURN_SPEED_SLOW
+                # twist.angular.z = TURN_SPEED_SLOW
+                self.pub.publish(twist)
 
+            if diff < angle_tolerance:  # Adjust tolerance as needed
+                twist.angular.z = 0.0  # Stop turning
+                self.pub.publish(twist)
+                # Debug information
+                self.get_logger().info('Reached target yaw. Stopping rotation.')
+                self.is_turning_180 = True
+                break
+
+            rclpy.spin_once(self, timeout_sec=0.1)
+
+    def is_rotating(self):
+        twist = Twist()
+        if twist.angular.z > ROTATE_THRETHOLD:
+            self.get_logger().info(f'Still rotating: {twist.angular.z:.2f}, Threthold: {ROTATE_THRETHOLD:.5f}')
+            return True
+        else:
+            self.get_logger().info(f'Stop rotating: {twist.angular.z:.2f}, Threthold: {ROTATE_THRETHOLD:.5f}')
+            return False      
+
+    def is_moving(self):
+        twist = Twist()
+        if twist.linear.x > MOVE_THRETHOLD:
+            self.get_logger().info(f'Still moving: {twist.linear.x:.2f}, Threthold: {MOVE_THRETHOLD:.5f}')
+            return True
+        else:
+            self.get_logger().info(f'Stop moving: {twist.linear.x:.2f}, Threthold: {MOVE_THRETHOLD:.5f}')
+            return False 
+
+    def move_forward_to_position(self, target_x, target_y, tolerance=0.1):
+        if not self.current_pose:
+            self.get_logger().info('Current pose not available.')
+            return
+
+        # Calculate the initial distance to the target
+        initial_position = self.current_pose.position
+        distance_to_target = math.sqrt(
+            (target_x - initial_position.x) ** 2 + 
+            (target_y - initial_position.y) ** 2)
+
+        self.get_logger().info(f'Initial distance to target: {distance_to_target:.2f} meters')
+
+        twist = Twist()
+        twist.linear.x = FORWARD_SPEED
+        self.pub.publish(twist)
+
+        while rclpy.ok():
+            current_position = self.current_pose.position
+            distance_to_target = math.sqrt(
+                (target_x - current_position.x) ** 2 + 
+                (target_y - current_position.y) ** 2)
+
+            # Debug information
+            self.get_logger().info(f'Robot Pose: ({current_position.x:.2f}, {current_position.y:.2f}) target: ({target_x:.2f}, {target_y:.2f}) Distance to target: {distance_to_target:.2f} meters')
+
+            if distance_to_target < tolerance:  # Stop if within tolerance
+                twist.linear.x = 0.0  # Stop moving forward
+                self.pub.publish(twist)
+                self.get_logger().info(f'Reached target position ({target_x:.2f}, {target_y:.2f}). Stopping.')
+                break
+
+            rclpy.spin_once(self, timeout_sec=0.1)
+
+
+    def run(self):
+        while rclpy.ok():
+            if not self.is_turning_180:
+                self.turn_around(math.pi)  # Turn 180 degrees
+            else:
+                # empty run for 1 s   TODO
+                # Move to the target position after turning around
+                if not self.is_rotating():
+                    # empty run for 1s
+                    current_yaw = self.get_yaw_from_pose(self.current_pose)
+                    self.get_logger().info(f'Current yaw: {current_yaw:.2f}')
+
+                    self.move_forward_to_position(TARGET_POS1_X, TARGET_POS1_Y)
+                    # break  # Exit the loop once the target position is reached
+                    if not self.is_moving():
+                        self.turn_around(WALL_ANGLE, True)
+                        if not self.is_rotating():
+                            current_yaw = self.get_yaw_from_pose(self.current_pose)
+                            self.get_logger().info(f'Current yaw: {current_yaw:.2f}')
+                            self.move_forward_to_position(TARGET_POS2_X, TARGET_POS2_Y)
+                        
+                            break
+
+            rclpy.spin_once(self, timeout_sec=0.1)
+
+def main(args=None):
+    rclpy.init(args=args)
+    auto_move_bot = AutoMoveBot()
+    
+    try:
+        auto_move_bot.run()
+    except KeyboardInterrupt:
+        auto_move_bot.get_logger().info('Node was stopped manually.')
+    finally:
+        auto_move_bot.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
